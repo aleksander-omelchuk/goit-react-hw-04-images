c390f49e1eda3a508cdb697cc2951bfff065702f
aleksander-omelchuk <aleksander.omelchuk@gmail.com>
2023-01-17 13:14:31 +0200

    change number

--- src/services/API.js
+++ src/services/API.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
 const API_KEY = '11240134-58b8f655e9e0f8ae8b6e8e7de';
-const IMG_PER_PAGE = 9;
+const IMG_PER_PAGE = 24;
 
 async function findImages(query, page = 1) {
   const url = `?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${IMG_PER_PAGE}`;
